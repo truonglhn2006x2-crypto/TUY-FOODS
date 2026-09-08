4c03085b7a3778efc4cc98ad7806eb8115921aba
truonglhn2006x2-crypto <truonglhn2006x2@gmail.com>
2026-09-08 10:46:46 +0700

    Add database

---
+++ database/data.sql
@@ -0,0 +1,43 @@
+-- Đồ ăn vặt (category_id = 1)
+INSERT INTO products (name, price, category_id, available) VALUES
+('Chân gà sốt Thái', 45000, 1, true),
+('Nem chua rán', 40000, 1, true),
+('Cá viên xiên', 30000, 1, true),
+('Takoyaki', 30000, 1, true),
+('Mì Indomi', 15000, 1, true),
+('Ngô xào', 15000, 1, true),
+('Há cảo chiên', 30000, 1, true),
+('Bánh tráng cuộn', 25000, 1, true),
+('Bánh tráng nướng', 15000, 1, true),
+('Khô bò lá lốt nướng', 10000, 1, true),
+('Gà rán', 55000, 1, true),
+('Phô mai que', 30000, 1, true),
+('Khoai tây chiên', 25000, 1, true),
+('Xúc xích', 10000, 1, true),
+('Lạp xưởng nướng đá', 15000, 1, true),
+('Dồi sụn', 10000, 1, true),
+('Xoài dầm', 20000, 1, true),
+('Ngô chiên', 20000, 1, true),
+('Bánh bao chay', 10000, 1, true),
+('Bánh mì bơ tỏi', 15000, 1, true),
+('Khô gà vị lá chanh', 300000, 1, true),
+('Sữa tươi chiên', 45000, 1, true);
+
+-- Đồ uống (category_id = 2)
+INSERT INTO products (name, price, category_id, available) VALUES
+('Trà sữa cốm', 55000, 2, true),
+('Trà tắc', 15000, 2, true),
+('Trà chanh', 15000, 2, true),
+('Trà hoa quả', 45000, 2, true),
+('Matcha latte', 25000, 2, true),
+('Rau má', 20000, 2, true),
+('Trà Thái', 22000, 2, true),
+('Nước sâm', 10000, 2, true);
+
+-- Sinh tố (category_id = 3)
+INSERT INTO products (name, price, category_id, available) VALUES
+('Sinh tố bơ', 40000, 3, true),
+('Sinh tố mãng cầu', 40000, 3, true),
+('Sinh tố xoài', 40000, 3, true),
+('Sinh tố dâu', 40000, 3, true),
+('Sinh tố dừa', 40000, 3, true);
