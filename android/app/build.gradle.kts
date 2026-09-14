519d8ee7665e274afde6c9b2ec49e2a491c3957b
Liam <iamqua444@gmail.com>
2026-09-14 23:45:05 +0700

    Them lich su don hang

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.tuyfood"
+
     compileSdk {
         version = release(37)
     }
@@ -15,7 +16,8 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -25,27 +27,56 @@ android {
             }
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility =
+            JavaVersion.VERSION_11
+
+        targetCompatibility =
+            JavaVersion.VERSION_11
     }
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.12.0")
+
+    // Thư viện giao diện Android
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    // Retrofit gọi API
+    implementation(
+        "com.squareup.retrofit2:retrofit:2.9.0"
+    )
+
+    implementation(
+        "com.squareup.retrofit2:converter-gson:2.9.0"
+    )
+
+    implementation(
+        "com.squareup.okhttp3:okhttp:4.12.0"
+    )
+
+    // Gson lưu lịch sử đơn hàng
+    implementation(
+        "com.google.code.gson:gson:2.10.1"
+    )
+
+    // RecyclerView
+    implementation(
+        "androidx.recyclerview:recyclerview:1.3.2"
+    )
+
+    // Testing
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
 
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    androidTestImplementation(
+        libs.androidx.espresso.core
+    )
 
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    androidTestImplementation(
+        libs.androidx.junit
+    )
 }
